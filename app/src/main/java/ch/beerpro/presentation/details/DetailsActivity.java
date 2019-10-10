ddd3e67ebdbd4c6533055599198509d04ca78d17
Dominic Gabriel <domi94@gmx.ch>
2019-10-10 22:45:13 +0200

    Added Detail Beer Activity Personal Average Rating and deep linking for beers

--- app/src/main/java/ch/beerpro/presentation/details/DetailsActivity.java
+++ app/src/main/java/ch/beerpro/presentation/details/DetailsActivity.java
@@ -5,8 +5,12 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +20,7 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.widget.NestedScrollView;
@@ -29,6 +34,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -97,6 +103,7 @@ public class DetailsActivity extends AppCompatActivity implements OnRatingLikedL
     private DetailsViewModel model;
     private String beerId;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -109,7 +116,20 @@ public class DetailsActivity extends AppCompatActivity implements OnRatingLikedL
                 .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         toolbar.setTitleTextColor(Color.alpha(0));
 
-        beerId = getIntent().getExtras().getString(ITEM_ID, "");
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        Uri data = intent.getData();
+        if (data != null && data.isHierarchical()) {
+            String uri = this.getIntent().getDataString();
+            Log.i("MyApp", "Deep link clicked " + uri);
+            if (Pattern.compile("https://www.beerpro.ch/beer/[a-zA-Z0-9]*$").matcher(uri).matches()) {
+                beerId = uri.replaceAll("https://www.beerpro.ch/beer/", "");
+            } else {
+                // TODO: handle error and display 404 beer not found activity?
+            }
+        } else {
+            beerId = getIntent().getExtras().getString(ITEM_ID, "");
+        }
 
         model = ViewModelProviders.of(this).get(DetailsViewModel.class);
         model.setBeerId(beerId);
@@ -123,14 +143,36 @@ public class DetailsActivity extends AppCompatActivity implements OnRatingLikedL
         model.getBeer().observe(this, this::updateBeer);
         model.getRatings().observe(this, this::updateRatings);
         model.getWish().observe(this, this::toggleWishlistView);
+        model.getMyRatings().observe(this, this::setPersonalAvgRating);
 
         recyclerView.setAdapter(adapter);
-        addRatingBar.setOnRatingBarChangeListener(this::addNewRating);
 
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         editNote.setOnClickListener(getNoteListener());
         changeVisibilityOfNoteField(settings);
         updateNote(settings);
+
+    }
+
+    private void setPersonalAvgRating(List<Rating> ratings) {
+        float ratingSum = 0;
+        int ratingCounter = 0;
+        for (Rating rating: ratings) {
+            if (rating.getBeerId().equals(beerId)) {
+                ratingSum += rating.getRating();
+                ratingCounter++;
+            }
+        }
+        float ratingAvg = ratingSum / ratingCounter;
+        setPersonalAvgRating(ratingAvg);
+    }
+
+    private void setPersonalAvgRating(float rating) {
+        if (rating == -1) {
+            addRatingBar.setRating(0);
+        } else {
+            addRatingBar.setRating(rating);
+        }
     }
 
     private void addNewRating(RatingBar ratingBar, float v, boolean b) {
@@ -139,6 +181,22 @@ public class DetailsActivity extends AppCompatActivity implements OnRatingLikedL
         intent.putExtra(CreateRatingActivity.RATING, v);
         ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this, addRatingBar, "rating");
         startActivity(intent, options.toBundle());
+    }
+
+    @OnClick(R.id.shareButton)
+    public void shareBeer() {
+        final Intent intent = new Intent(Intent.ACTION_SEND);
+        final String uri = "https://www.beerpro.ch/beer/" + beerId;
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "Let's share this beer: " + uri + "\n" + manufacturer.getText() + "\n" + name.getText());
+
+        try {
+            startActivity(Intent.createChooser(intent, "Select an action"));
+        } catch (android.content.ActivityNotFoundException ex) {
+            // TODO: (handle error)
+        }
+
+
     }
 
     @OnClick(R.id.actionsButton)
@@ -150,6 +208,22 @@ public class DetailsActivity extends AppCompatActivity implements OnRatingLikedL
 
         View addPrivateNote = view.findViewById(R.id.addPrivateNote);
         addPrivateNote.setOnClickListener(getNoteListener());
+
+        View addRating = view.findViewById(R.id.addRating);
+        addRating.setOnClickListener(getRatingListener());
+    }
+
+
+    private View.OnClickListener getRatingListener() {
+        return view -> {
+            showRatingActivity(view.getContext());
+        };
+    }
+
+    private void showRatingActivity(Context context) {
+            Intent intent = new Intent(context, CreateRatingActivity.class);
+            intent.putExtra(CreateRatingActivity.ITEM, model.getBeer().getValue());
+            startActivity(intent);
     }
 
     private View.OnClickListener getNoteListener() {
--- app/src/main/java/ch/beerpro/presentation/details/DetailsViewModel.java
+++ app/src/main/java/ch/beerpro/presentation/details/DetailsViewModel.java
@@ -1,12 +1,21 @@
 package ch.beerpro.presentation.details;
 
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.android.gms.tasks.Task;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import ch.beerpro.data.repositories.BeersRepository;
 import ch.beerpro.data.repositories.CurrentUser;
@@ -20,25 +29,29 @@ import ch.beerpro.domain.models.Wish;
 public class DetailsViewModel extends ViewModel implements CurrentUser {
 
     private final MutableLiveData<String> beerId = new MutableLiveData<>();
+    private final MutableLiveData<String> currentUserId = new MutableLiveData<>();
     private final LiveData<Beer> beer;
     private final LiveData<List<Rating>> ratings;
+    private final LiveData<List<Rating>> myRatings;
     private final LiveData<Wish> wish;
 
     private final LikesRepository likesRepository;
     private final WishlistRepository wishlistRepository;
+    private final RatingsRepository ratingsRepository;
 
     public DetailsViewModel() {
         // TODO We should really be injecting these!
         BeersRepository beersRepository = new BeersRepository();
-        RatingsRepository ratingsRepository = new RatingsRepository();
+        ratingsRepository = new RatingsRepository();
         likesRepository = new LikesRepository();
         wishlistRepository = new WishlistRepository();
 
-        MutableLiveData<String> currentUserId = new MutableLiveData<>();
+//        MutableLiveData<String> currentUserId = new MutableLiveData<>();
         beer = beersRepository.getBeer(beerId);
         wish = wishlistRepository.getMyWishForBeer(currentUserId, getBeer());
         ratings = ratingsRepository.getRatingsForBeer(beerId);
         currentUserId.setValue(getCurrentUser().getUid());
+        myRatings = ratingsRepository.getMyRatings(currentUserId);
     }
 
     public LiveData<Beer> getBeer() {
@@ -52,6 +65,8 @@ public class DetailsViewModel extends ViewModel implements CurrentUser {
     public LiveData<List<Rating>> getRatings() {
         return ratings;
     }
+
+    public LiveData<List<Rating>> getMyRatings() { return myRatings; }
 
     public void setBeerId(String beerId) {
         this.beerId.setValue(beerId);
